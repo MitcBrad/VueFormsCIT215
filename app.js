042ed1d413fdd5e4b0523557514b94f5aeb94e79
BradMitchell <32003566+MitcBrad@users.noreply.github.com>
2021-11-08 06:37:23 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,14 +3,47 @@ new Vue({
     data: {
         name: "",
         email: "",
-        emailRules: [ v => /.+@.+/.test(v) || 'Email does not meet requirements.' ]
+    },
+    methods:{
+        alert: function(){
+            alert("thank you for signing up " + this.name + ", your account has been created with the email " + this.email )
+        }
+    },
+
+})
+
+
+new Vue({
+    el: "#toggle",
+    data: {
+        toggles:[
+            {
+                name:"Protagonist",
+                codename:"Joker"},
+            {
+                name:"Anne",
+                codename:"Panther"},
+            {
+                name:"Ryuji",
+                codename:"Skull"}
+        ]
+    },methods: {
+
     }
 })
 
-function clicked(){
-    alert("Your name requires at least 2 characters.\nYour email requires an @ and .");
-}
 
+let starter = true;
+document.addEventListener("DOMContentLoaded", () =>{
+    if (starter == true){
+        let nameheader = document.getElementsByClassName("header");
+        nameheader.setAttribute("name", "codename");
 
+        starter = false;
+    }
+});
 // Input: Name (text field). The input needs to have at least 2 characters. Required.
 // Input: Email (text field). The input needs to be a valid email address. Required.
+
+// Part 2: Using [{name:"Protagonist",codename:"Joker"},{name:"Anne",codename:"Panther"},{name:"Ryuji",codename:"Skull"}],
+// create a list of components that have unique names and that when I click on them, it toggles between the name and codename.
